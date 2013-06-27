6b7bd0a9dbd9383ecc071f6a01f927630ccef2e9
F. Sena <github@senartes.com.br>
2013-06-27 11:31:34 -0300

    First Comit

---
+++ libraries/Flickr.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once APPPATH."/third_party/phpFlickr/phpFlickr.php"; 
+ 
+class Flickr extends phpFlickr {
+
+    const FLICKR_API_KEY = 'your_api_key';
+    const FLICKR_API_SECRET = 'yout_api_secret'; 
+
+    public function __construct($api_key = self::FLICKR_API_KEY, $secret = self::FLICKR_API_SECRET, $die_on_error = false){
+        parent::__construct($api_key, $secret, $die_on_error);
+
+        $ci =& get_instance();
+
+        if($ci->db->dbdriver == 'mysqli'
+            || $ci->db->dbdriver == 'mysql') {
+            $this->enableCache('db', 'mysql://' . $ci->db->username . ':' . $ci->db->password . '@' . $ci->db->hostname . '/' . $ci->db->database);
+        } else{
+            $this->enableCache('fs', dirname(__FILE__) . '/cache');
+        }
+    }
+}
